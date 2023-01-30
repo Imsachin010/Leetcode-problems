e49cce34dbdc1a55ba37e109d0c7c72d24844052
sachin <007mishrasachinmishra@gmail.com>
2023-01-30 23:05:11 +0530

    day 19

---
+++ N-th Tribonacci Number/1137.py
@@ -0,0 +1,10 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        t = [0] * (n+5)
+        t[0] =0
+        t[1] = 1
+        t[2] = 1
+        for i in range(3,n+1):
+            t[i] = t[i-3] + t[i-2] + t[i-1]
+        return t[n]
