17e0085fef31a0d2901a18b189cf5a9754029624
sachin <007mishrasachinmishra@gmail.com>
2023-02-03 23:57:28 +0530

    day 23

---
+++ Zigzag Conversion/6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows ==1:
+            return s
+        
+        rows = [[] for _ in range(numRows)]
+
+        row = 0
+        direction = 0
+        for c in s:
+            rows[row].append(c) 
+            row += direction
+
+            if row ==0 or row ==numRows - 1:
+                direction *= 1
+
+        ans = ["".join(row) for row in rows]
+        return "".join(ans)
