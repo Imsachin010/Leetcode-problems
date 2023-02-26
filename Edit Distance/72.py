0999c92aa58b81a72869fd1b5726a388b82f2c74
sachin <007mishrasachinmishra@gmail.com>
2023-02-26 23:44:09 +0530

    day 46

--- Edit Distance/72.py
+++ Edit Distance/72.py
@@ -3,10 +3,10 @@ class Solution:
         N1, N2 = len(word1), len(word2)
         INF = 10 ** 20
 
-        has_cache = [[False] * (N2 + 1) for _ in range(N1+1)]
-        cache = [[None] * (N2+1) for _ in range(N1+1)]
+        has_cache = [[False] * (N2 + 1) for _ in range(N1 + 1)]
+        cache = [[None] * (N2 + 1) for _ in range(N1 + 1)]
 
-        def minDist(index1,index2):
+        def minDist(index1, index2):
             if index1 == N1:
                 return N2 - index2
             if index2 == N2:
@@ -16,13 +16,14 @@ class Solution:
                 return cache[index1][index2]
             
             best = INF
-            if word[index1] == word2[index2]:
-                best= min(best, minDist(index1, index2 +1) +1)
+            if word1[index1] == word2[index2]:
+                best= min(best, minDist(index1 + 1, index2 + 1))
             
-            best = min(best, minDist(index1, index2+1)+1)
-            best = min(best, minDist(index1+1, index2)+1)
-            best = min(best, minDist(index1+1, index2+1)+1)
+            best = min(best, minDist(index1, index2 + 1) + 1)
+            best = min(best, minDist(index1 + 1, index2) + 1)
+            best = min(best, minDist(index1 + 1, index2 + 1) + 1)
 
             has_cache[index1][index2] = True
             cache[index1][index2] = best
             return best
+            return minDist(0, 0)
